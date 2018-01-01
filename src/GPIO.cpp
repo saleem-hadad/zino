bb0d1424618e4c56d1c0e90baaa210c05587bd88
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-01 09:43:26 +0800

    fix intendation

--- src/GPIO.cpp
+++ src/GPIO.cpp
@@ -24,44 +24,43 @@ unsigned char *PIN_B  = (unsigned char*) 0x23;
 
 void GPIO::setup(Pin& pin, PinMode mode)
 {
-  if(mode == Output)
-  {
-    *DDR_B |= 1 << pin.pin();
-  }
-  else if(mode == Input)
-  {
-    *DDR_B &= ~(1 << pin.pin());
-  }
-  else if(mode == InputWithPullUp)
-  {
-    *DDR_B &= ~(1 << pin.pin());
-    *PORT_B |= (1 << pin.pin());
-  }
+    if(mode == Output)
+    {
+        *DDR_B |= 1 << pin.pin();
+    }
+    else if(mode == Input)
+    {
+        *DDR_B &= ~(1 << pin.pin());
+    }
+    else if(mode == InputWithPullUp)
+    {
+        *DDR_B &= ~(1 << pin.pin());
+        *PORT_B |= (1 << pin.pin());
+    }
 }
 
 void GPIO::write(Pin& pin, char value)
 {
-  if(value)
-  {
-    *PORT_B |= (1 << pin.pin());
-  }
-  else
-  {
+    if(value)
+    {
+        *PORT_B |= (1 << pin.pin());
+        return;
+    }
+
     *PORT_B &= ~(1 << pin.pin());
-  }
 }
 
 void GPIO::high(Pin& pin)
 {
-  GPIO::write(pin, 1);
+    GPIO::write(pin, 1);
 }
 
 void GPIO::low(Pin& pin)
 {
-  GPIO::write(pin, 0);
+    GPIO::write(pin, 0);
 }
 
 bool GPIO::read(Pin& pin)
 {
-  return (*PIN_B & (1 << pin.pin()));
+    return (*PIN_B & (1 << pin.pin()));
 }
