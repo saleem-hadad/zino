5071bf5af64301cde86840636044a1d6a713737f
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-01 08:38:20 +0800

    fix typo

--- src/Blinky.h
+++ src/Blinky.h
@@ -26,27 +26,27 @@ class Blinky
 {
 public:
 	/*
-    Blinky constructor:
-    @params:
-    void
-    @return:
-    void
-  */
+	Blinky constructor:
+	@params:
+	void
+	@return:
+	void
+	*/
 	Blinky();
 
 	/*
     init method:
     @params:
     Pin pin
-		unsigned int onTime
-		unsigned int offTime
+	unsigned int onTime
+	unsigned int offTime
     @return:
     void
-		---
-		Used to update the default settings of the Blinky
-		object. Please note that the onTime and offTime
-		both are in ms, e.g. 1 s = to 1000 ms.
-  */
+	---
+	Used to update the default settings of the Blinky
+	object. Please note that the onTime and offTime
+	both are in ms, e.g. 1 s = to 1000 ms.
+  	*/
 	void init(Pin pin, unsigned int onTime, unsigned int offTime);
 
 	/*
@@ -57,21 +57,20 @@ public:
     void
     ---
     Should be placed in the main loop of the program.
-  */
+  	*/
 	void refresh(void);
-
 
 private:
 	// The pin object used to be set as output.
 	Pin* _pin;
 
-	// Used to handle toggelling functionality.
+	// Used to handle toggling functionality.
 	bool active = false;
 
-	// The ammount of time the pin's status is high.
+	// The amount of time the pin's status is high.
   unsigned int onTime;
 
-	// The ammount of time the pin's status is low.
+	// The amount of time the pin's status is low.
   unsigned int offTime;
 
 	// Used to track how many ms passed.
