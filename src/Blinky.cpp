9c72a91a15c18af88cbee2bb740a97482f8fd6f1
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-01 08:43:29 +0800

    fix intendation

--- src/Blinky.cpp
+++ src/Blinky.cpp
@@ -21,38 +21,38 @@
 
 Blinky::Blinky()
 {
-  //
+    //
 }
 
 void Blinky::init(Pin pin, unsigned int onTime, unsigned int offTime)
 {
-  this->_pin    = &pin;
-  this->onTime  = onTime;
-  this->offTime = offTime;
+    this->_pin    = &pin;
+    this->onTime  = onTime;
+    this->offTime = offTime;
 
-  GPIO::setup(pin, Output);
+    GPIO::setup(pin, Output);
 }
 
 void Blinky::refresh(void)
 {
-  unsigned long currentTime = millis();
+    unsigned long currentTime = millis();
 
-  if(! this->active)
-  {
-    if (currentTime - this->previousTime >= this->offTime)
+    if(! this->active)
     {
-      this->active = true;
-      this->previousTime = currentTime;
-      GPIO::high(*this->_pin);
+        if (currentTime - this->previousTime >= this->offTime)
+        {
+            this->active = true;
+            this->previousTime = currentTime;
+            GPIO::high(*this->_pin);
+        }
     }
-  }
-  else
-  {
-    if(currentTime - this->previousTime >= this->onTime)
+    else
     {
-      this->active = false;
-      this->previousTime = currentTime;
-      GPIO::low(*this->_pin);
+        if(currentTime - this->previousTime >= this->onTime)
+        {
+            this->active = false;
+            this->previousTime = currentTime;
+            GPIO::low(*this->_pin);
+        }
     }
-  }
 }
