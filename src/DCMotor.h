bd49f68243232ea4179ff425d86451bf262c4052
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-03 04:46:38 +0800

    update DCMotor docs

--- src/DCMotor.h
+++ src/DCMotor.h
@@ -38,14 +38,13 @@ public:
 	init method:
 	@params:
 	Pin pin
-	unsigned int onTime
-	unsigned int offTime
+	unsigned int frequency
 	@return:
 	void
 	---
 	Used to update the default settings of the DCMotor
-	object. Please note that the onTime and offTime
-	both are in ms, e.g. 1 s = to 1000 ms.
+	object. Please note that the frequency is in hz
+    remember: T=1/f, e.g. f=100, period(T)=1/100
 	*/
 	void init(Pin pin, unsigned int frequency);
 
@@ -60,17 +59,31 @@ public:
 	*/
 	void refresh(void);
 
+    /*
+	setDutyCycle method:
+	@params:
+	Pin pin
+	double dutyCycle
+	@return:
+	void
+	---
+	Used to update the dc motor DC. For example if the
+    frequency set to 1(period=1s), setting DC = 0.5
+    the pin will stay on for 0.5s and off for 0.5s
+	*/
     void setDutyCycle(double dutyCycle);
 private:
 	// The pin object used to be set as output.
 	Pin* _pin;
 
+    // Used to generate PWM signal on the given pin.
     Blinky _blinker;
+
+    // Used to determin the on and off time of the blinker.
+    unsigned long _period = 1;
 
 	// Used to track if the blinky object initialized or not.
 	bool _initialized = false;
-
-	unsigned long _period = 1;
 };
 
 #endif
