53cdee20ea2e17f13d49288462f90e65f4ef1023
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-03 04:35:44 +0800

    add DCMotor class

---
+++ src/DCMotor.cpp
@@ -0,0 +1,48 @@
+/*
+ .----------------.  .----------------.  .-----------------. .----------------.
+| .--------------. || .--------------. || .--------------. || .--------------. |
+| |   ________   | || |     _____    | || | ____  _____  | || |     ____     | |
+| |  |  __   _|  | || |    |_   _|   | || ||_   \|_   _| | || |   .'    `.   | |
+| |  |_/  / /    | || |      | |     | || |  |   \ | |   | || |  /  .--.  \  | |
+| |     .'.' _   | || |      | |     | || |  | |\ \| |   | || |  | |    | |  | |
+| |   _/ /__/ |  | || |     _| |_    | || | _| |_\   |_  | || |  \  `--'  /  | |
+| |  |________|  | || |    |_____|   | || ||_____|\____| | || |   `.____.'   | |
+| |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'
+
+ Created by: Saleem Hadad
+ Date: 3/1/2018
+ Github: https://github.com/saleem-hadad/zino
+*/
+
+#include "DCMotor.h"
+#include <Arduino.h>
+#include "GPIO.h"
+
+DCMotor::DCMotor(){}
+
+void DCMotor::init(Pin pin, unsigned int frequency)
+{
+
+    this->_pin = &pin;
+    this->_period = 1000 / frequency;
+    this->_blinker.init(pin, 0, this->_period);
+
+    this->_initialized = true;
+}
+
+void DCMotor::refresh(void)
+{
+    if(! this->_initialized) { return; }
+
+    this->_blinker.refresh();
+}
+
+void DCMotor::setDutyCycle(double dutyCycle)
+{
+    if(! this->_initialized) { return; }
+
+    this->_blinker.setOnTime(this->_period * dutyCycle);
+    this->_blinker.setOffTime(this->_period - (this->_period * dutyCycle));
+}
--- src/DCMotor.h
+++ src/DCMotor.h
@@ -12,26 +12,27 @@
  '----------------'  '----------------'  '----------------'  '----------------'
 
  Created by: Saleem Hadad
- Date: 30/12/2017
+ Date: 3/1/2018
  Github: https://github.com/saleem-hadad/zino
 */
 
-#ifndef ZINO_BLINKY_H
-#define ZINO_BLINKY_H
+#ifndef ZINO_DCMOTOR_H
+#define ZINO_DCMOTOR_H
 
 #include "Pin.h"
+#include "Blinky.h"
 
-class Blinky
+class DCMotor
 {
 public:
 	/*
-	Blinky constructor:
+	DCMotor constructor:
 	@params:
 	void
 	@return:
 	void
 	*/
-	Blinky();
+	DCMotor();
 
 	/*
 	init method:
@@ -42,11 +43,11 @@ public:
 	@return:
 	void
 	---
-	Used to update the default settings of the Blinky
+	Used to update the default settings of the DCMotor
 	object. Please note that the onTime and offTime
 	both are in ms, e.g. 1 s = to 1000 ms.
 	*/
-	void init(Pin pin, unsigned int onTime, unsigned int offTime);
+	void init(Pin pin, unsigned int frequency);
 
 	/*
 	refresh method:
@@ -59,51 +60,17 @@ public:
 	*/
 	void refresh(void);
 
-	/*
-	setOnTime method:
-	@params:
-	Pin pin
-	unsigned int onTime
-	@return:
-	void
-	---
-	Used to update the amount of time the pin will
-	set as High. Please note that the value of
-	onTime in ms, e.g. 1 s equal to 1000 ms.
-	*/
-	void setOnTime(unsigned int onTime);
-
-	/*
-	setOnTime method:
-	@params:
-	Pin pin
-	unsigned int onTime
-	@return:
-	void
-	---
-	Used to update the amount of time the pin will
-	set as Low. Please note that the value of
-	offTime in ms, e.g. 1 s = to 1000 ms.
-	*/
-	void setOffTime(unsigned int offTime);
+    void setDutyCycle(double dutyCycle);
 private:
 	// The pin object used to be set as output.
 	Pin* _pin;
 
+    Blinky _blinker;
+
 	// Used to track if the blinky object initialized or not.
 	bool _initialized = false;
 
-	// Used to handle toggling functionality.
-	bool _active = false;
-
-	// The amount of time the pin's status is high.
-	unsigned int _onTime;
-
-	// The amount of time the pin's status is low.
-	unsigned int _offTime;
-
-	// Used to track how many ms passed.
-	unsigned long _previousTime = 0;
+	unsigned long _period = 1;
 };
 
 #endif
