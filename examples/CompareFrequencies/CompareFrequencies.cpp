117280fa467d1cc467578b75a2649cfd6e20c181
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-03 23:46:44 +0800

    update CompareFrequencies example

--- examples/CompareFrequencies/CompareFrequencies.cpp
+++ examples/CompareFrequencies/CompareFrequencies.cpp
@@ -9,18 +9,20 @@ LED led;
 void setup()
 {
     led.init(ledPin);
+
+    while(1)
+    {
+        // freq. ~= 147 Khz
+        digitalWrite(8, 1);
+        digitalWrite(8, 0);
+
+        // freq. ~= 200 Khz
+        led.toggle();
+
+        // freq. ~= 440 Khz
+        GPIO::write(ledPin, 1);
+        GPIO::write(ledPin, 0);
+    }
 }
 
-void loop()
-{
-    // freq. ~= 140 Khz
-    digitalWrite(8, 1);
-    digitalWrite(8, 0);
-
-    // freq. ~= 185 Khz
-    led.toggle();
-
-    // freq. ~= 400 Khz
-    GPIO::write(ledPin, 1);
-    GPIO::write(ledPin, 0);
-}
+void loop(){}
